5cbd51a27ee668c0576b4f70448b40488512a9cd
ydang <ydang@futurify.vn>
2019-11-30 18:35:08 +0700

    Add Product image controller

---
+++ OnlineShop/OnlineShop.ProductAPI/Services/ProductImageService.cs
@@ -0,0 +1,19 @@
+﻿using OnlineShop.ProductAPI.Models;
+using OnlineShop.ProductAPI.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ProductAPI.Services
+{
+    public class ProductImageService : IProductImageService
+    {
+        private readonly ProductContext _context;
+
+        public ProductImageService(ProductContext context)
+        {
+            _context = context;
+        }
+    }
+}
