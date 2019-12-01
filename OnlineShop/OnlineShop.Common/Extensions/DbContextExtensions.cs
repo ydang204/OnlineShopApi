d7abfcd635effb68da2b739c7d6c101dd9e343c0
ydang <ydang@futurify.vn>
2019-12-01 22:59:42 +0700

    Correct ModifiedBy data

--- OnlineShop/OnlineShop.Common/Extensions/DbContextExtensions.cs
+++ OnlineShop/OnlineShop.Common/Extensions/DbContextExtensions.cs
@@ -34,26 +34,32 @@ namespace OnlineShop.Common.Extensions
                 {
                     if (entry.Metadata.FindProperty("CreatedAt") != null)
                     {
-                        if (entry.Property("CreatedAt").CurrentValue == null)
+                        if (entry.Property("CreatedAt").CurrentValue == null ||
+                            (DateTime)entry.Property("CreatedAt").CurrentValue == default
+                            )
                             entry.Property("CreatedAt").CurrentValue = currentTime;
                     }
 
                     if (entry.Metadata.FindProperty("CreatedBy") != null)
                     {
-                        if (entry.Property("CreatedBy").CurrentValue == null || (int)entry.Property("CreatedBy").CurrentValue == 0)
+                        if (entry.Property("CreatedBy").CurrentValue == null ||
+                            (int)entry.Property("CreatedBy").CurrentValue == default)
                             entry.Property("CreatedBy").CurrentValue = accountId;
                     }
                 }
 
                 if (entry.Metadata.FindProperty("ModifiedAt") != null)
                 {
-                    if (entry.Property("ModifiedAt").CurrentValue == null)
+                    if (entry.Property("ModifiedAt").CurrentValue == null ||
+                        (DateTime)entry.Property("ModifiedAt").CurrentValue == default
+                        )
                         entry.Property("ModifiedAt").CurrentValue = currentTime;
                 }
 
-                if (entry.Metadata.FindProperty("ModifiedBy") != null || (int)entry.Property("CreatedBy").CurrentValue == 0)
+                if (entry.Metadata.FindProperty("ModifiedBy") != null)
                 {
-                    if (entry.Property("ModifiedBy").CurrentValue == null)
+                    if (entry.Property("ModifiedBy").CurrentValue == null ||
+                    (int)entry.Property("ModifiedBy").CurrentValue == default)
                         entry.Property("ModifiedBy").CurrentValue = accountId;
                 }
             }
