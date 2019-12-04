ea22cd776887c1b2a9b1a1256303b7246da51bf2
ydang <ydang@futurify.vn>
2019-12-04 23:15:02 +0700

    Add MoMo controller

---
+++ OnlineShop/OnlineShop.OrderAPI/Controllers/MomoPaymentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.OrderAPI.Controllers
+{
+    [Route("api/momo-payment")]
+    [ApiController]
+    //[Authorize]
+    public class MomoPaymentController : ControllerBase
+    {
+
+    }
+}
